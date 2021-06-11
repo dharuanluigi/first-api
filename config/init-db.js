9f499ce4abbb34685027423db9b58b4956f79877
Dharuan Luigi <dharuanluigi@gmail.com>
2021-06-10 22:54:51 -0300

    Added script to setup database

---
+++ config/init-db.js
@@ -0,0 +1,53 @@
+const Database = require('./db')
+
+const initDB = {
+    async init() {
+        await this.createTableProducts()
+        await this.createTableOrder()
+    },
+    async createTableProducts() {
+        console.time()
+        const connector = await Database()
+
+        await connector.run(`
+            DROP TABLE IF EXISTS Products;
+        `)
+
+        await connector.run(`
+            CREATE TABLE Products (
+                id_prod INTEGER AUTO INCREMENT PRIMARY KEY,
+                name VARCHAR(45),
+                value FLOAT
+            );
+        `)
+
+        await connector.close()
+
+        console.timeEnd()
+        console.info('Table PRODUCTS, created successfully!')
+    },
+    async createTableOrder() {
+        console.time()
+        const connector = await Database()
+
+        await connector.run(`
+            DROP TABLE IF EXISTS Orders;
+        `)
+
+        await connector.run(`
+            CREATE TABLE Orders (
+                id_ord INTEGER AUTO INCREMENT PRIMARY KEY,
+                id_prod INTEGER,
+                qtd INTEGER,
+                FOREIGN KEY (id_prod) REFERENCES Products (id_prod)
+            );
+        `)
+        
+        await connector.close()
+
+        console.timeEnd()
+        console.info('Table ORDERS, created successfully!')
+    }
+}
+
+initDB.init()
