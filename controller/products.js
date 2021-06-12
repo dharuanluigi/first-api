25296173548ec12bdec34cc3c3991acc0d7ca7ed
Dharuan Luigi <dharuanluigi@gmail.com>
2021-06-12 18:53:43 -0300

    Added get a prod function and 3 more

--- controller/products.js
+++ controller/products.js
@@ -4,7 +4,16 @@ module.exports = {
     async getAll() {
         return await productModel.getAll()
     },
+    async get(prod_id) {
+        return await productModel.get(prod_id)
+    },
     async addProd(prod_data) {
-        return await productModel.setA(prod_data)
+        return await productModel.set(prod_data)
+    },
+    async updateProd(prod_data) {
+        return await productModel.update(prod_data)
+    },
+    async deleteProd(prod_id) {
+        return await productModel.delete(prod_id)
     }
 }
