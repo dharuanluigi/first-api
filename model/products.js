ccc433dc162bf373d1a6cf973de2c10f5933e63b
Dharuan Luigi <dharuanluigi@gmail.com>
2021-06-12 01:04:09 -0300

    Patter returning declaration variables

--- model/products.js
+++ model/products.js
@@ -2,39 +2,71 @@ const Database = require('../config/db')
 
 module.exports = {
     async getAll() {
-        console.time()
 
+        let response = "Here we are, all products bellow"
+        let status = 200
+        let data = null
+
+        console.time()
         const connector = await Database()
 
-        const allPRDs = await connector.all(`
-            SELECT * FROM Products;
-        `)
-
-        await connector.close()
-
+        try {
+             data = await connector.all(`
+                SELECT * FROM Products;
+            `)
+        }
+        catch(err) {
+            response = "Problem when try to get all products!"
+            status = 500
+        }
+        finally {
+            await connector.close()
+        }
         console.timeEnd()
-        console.time('All products was got successfully!')
 
-        return allPRDs
+        return {
+            response,
+            status,
+            data: data === null ? "There nothing here!" : data
+        }
     },
     async setA(prod_data) {
+        
+        let response = "Product added successfully"
+        let status = 200
+
         console.time()
 
         const connector = await Database()
 
-        await connector.run(`
-            INSERT INTO Products VALUES (
-                NULL,
-                "${prod_data.name}",
-                ${prod_data.price}
-            );
-        `)
-
-        await connector.close()
+        try {
+            await connector.run(`
+                INSERT INTO Products (name, price) VALUES (
+                    "${prod_data.name}",
+                    ${prod_data.price}
+                );
+            `)
+        }
+        catch(err) {
+            switch(err.errno) {
+                case 19:
+                    response = "Value already exist!"
+                    status = 409
+                    break
+                default:       
+                    response = "Unknow error!"
+                    status = 500
+            }
+        }
+        finally {
+            await connector.close()
+        }
 
         console.timeEnd()
-        console.info('Insert ended!')
 
-        return "Product"
+        return {
+            response,
+            status
+        }
     }
 }
